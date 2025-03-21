3d9d6708848b46593c0bf2fadedc16ee539ec2c2
Bananbka <bananbka0.2@gmail.com>
2025-03-21 21:22:58 +0200

    add admin dashboard with scanner

--- app.py
+++ app.py
@@ -8,15 +8,7 @@ app = create_app()
 app.register_blueprint(routes.routes)
 
 
-@app.route('/test_email', methods=['GET'])
-def test_email():
-    try:
-        msg = Message('Test Email', recipients=['nikitaz9251015@gmail.com'])
-        msg.body = "I'm testing email!"
-        mail.send(msg)
-        return "Email sent successfully!"
-    except Exception as e:
-        return f"Error sending email: {str(e)}"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ app/static/js/admin_dashboard.js
@@ -0,0 +1,56 @@
+﻿function domReady(fn) {
+    document.getElementById('my-qr-reader').style.display = 'none';
+    document.getElementById('hide-scanner-button').style.display = 'none';
+    if (
+        document.readyState === "complete" ||
+        document.readyState === "interactive"
+    ) {
+        setTimeout(fn, 1000);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+domReady(function () {
+
+    function onScanSuccess(decodeText, decodeResult) {
+        alert("You Qr is : " + decodeText + decodeResult);
+        console.log(decodeText);
+        fetch('/verify_qr', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                qr_data: decodeText
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            if (data.success) {
+                console.log("Success!!!");
+            }
+            else {
+                console.log("Error!!!" + data);
+            }
+        })
+        .catch(err => console.log(err));
+    }
+
+    let htmlscanner = new Html5QrcodeScanner(
+        "my-qr-reader",
+        { fps: 1, qrbos: 250 }
+    );
+    htmlscanner.render(onScanSuccess);
+});
+
+function showScanner() {
+    document.getElementById('my-qr-reader').style.display = 'block';
+    document.getElementById('show-scanner-button').style.display = 'none';
+    document.getElementById('hide-scanner-button').style.display = 'block';
+}
+function hideScanner() {
+    document.getElementById('my-qr-reader').style.display = 'none';
+    document.getElementById('hide-scanner-button').style.display = 'none';
+    document.getElementById('show-scanner-button').style.display = 'block';
+}
