8d2b487e84d663734facd450b85b150783384caf
Bananbka <bananbka0.2@gmail.com>
2025-03-23 00:18:29 +0200

    Add WayForPay module for API requests

---
+++ app/modules/WayForPay.py
@@ -0,0 +1,64 @@
+﻿import hashlib
+import hmac
+import requests
+
+
+class WayForPay:
+    API_URL = "https://api.wayforpay.com/api"
+
+    def __init__(self, account, key, domain):
+        self.account = account
+        self.key = key
+        self.domain = domain
+
+    def get_signature(self, data):
+        order_reference = data['orderReference']
+        order_date = data['orderDate']
+        amount = data['amount']
+        currency = data['currency']
+        product_names: list = data['productName']
+        product_counts: list = data['productCount']
+        product_prices: list = data['productPrice']
+
+        signature_text = (
+            f"{self.account};"
+            f"{self.domain};"
+            f"{order_reference};"
+            f"{order_date};"
+            f"{amount};"
+            f"{currency};"
+            f"{";".join(product_names)};"
+            f"{";".join(str(count) for count in product_counts)};"
+            f"{";".join(str(price) for price in product_prices)}"
+        )
+        signature = hmac.new(self.key.encode("utf-8"), signature_text.encode("utf-8"), hashlib.md5).hexdigest()
+        return signature
+
+
+    #serviceUrl - URL, на який система повинна відправляти повідомлення з результатом оплати інвойсу безпосередньо мерчанту
+    #При тесті з норм сайтом треба буде чекнути що там прилітає
+    def create_invoice(self, data):
+        try:
+            body = {
+                "merchantSignature": self.get_signature(data),
+                "merchantAccount": self.account,
+                "merchantDomainName": self.domain,
+                "transactionType": "CREATE_INVOICE",
+                "apiVersion": "1",
+                "language": "ua"
+            }
+            body.update(data)
+            headers = {"Content-Type": "application/json"}
+
+            response = requests.post(self.API_URL, json=body, headers=headers)
+
+            if response.text == "Api cannot read incoming request data. Unknown format":
+                raise ValueError("Unknown format")
+
+            return response.json()
+
+        except KeyError as e:
+            raise KeyError(f"No required argument – {e}")
+        except Exception as e:
+            raise e
+
---
+++ app/modules/__init__.py
@@ -0,0 +1,1 @@
+﻿from .WayForPay import WayForPay
