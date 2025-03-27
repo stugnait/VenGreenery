395f33362e42d5f1ef0101bbfc650fbcba419bac
Bananbka <bananbka0.2@gmail.com>
2025-03-27 19:55:04 +0200

    update flask secret key (get from .env)

--- app/config.py
+++ app/config.py
@@ -10,4 +10,4 @@ class Config:
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
     MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
-    SECRET_KEY="dev"
+    SECRET_KEY= os.getenv('SECRET_KEY')
